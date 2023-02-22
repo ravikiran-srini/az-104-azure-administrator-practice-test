962caae55a0fbd356b4c9df7320c6fe21e3d12d9
ravikiran-srini <39718717+ravikiran-srini@users.noreply.github.com>
2023-02-22 14:14:52 +0530

    Add files via upload

---
+++ Use azcopy to copy data with Azure AD and different container access levels - PS Command.ps1
@@ -0,0 +1,3 @@
+azcopy copy 'https://strdev011.blob.core.windows.net/source1' 'https://strdev012.blob.core.windows.net/target1' --recursive
+
+azcopy copy 'https://strdev011.blob.core.windows.net/source2' 'https://strdev012.blob.core.windows.net/target2' --recursive
